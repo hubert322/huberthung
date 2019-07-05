24da9db9200d1263caab44c70c16d9181246dd6b
hubert322 <hubert322@gmail.com>
2019-07-05 14:21:02 -0400

    Created basic layout of Experience page. Added section.css to make sections have consistent styling

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,13 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./Experience.css";
+import "../../assets/styles/section.css";
 
 function Experience(props) {
   const { setRef } = props;
 
   return (
-    <div className="Experience" ref={ref => setRef("Experience", ref)}>
-      <h2 className="Experience-title">Experience</h2>
+    <div className="section-container" ref={ref => setRef("Experience", ref)}>
+      <h2 className="section-title">Experience</h2>
+      <div className="section-content">
+        <div className="Experience-main-card">
+          <h3 className="Experience-card-title">Learning A-Z</h3>
+        </div>
+        <div className="Experience-sub-div">
+          <div className="Experience-sub-card">
+            <h4 className="Experience-card-title">Data Analysis on Canvas</h4>
+          </div>
+          <div className="Experience-sub-card">
+            <h4 className="Experience-card-title">Mobile Phone Laser Tag</h4>
+          </div>
+          <div className="Experience-sub-card">
+            <h4 className="Experience-card-title">A Light in the Darkness</h4>
+          </div>
+          <div className="Experience-sub-card">
+            <h4 className="Experience-card-title">Rube Goldberg Machine</h4>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
