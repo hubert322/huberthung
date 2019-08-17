5100f67fa4a65b4f48ac6562d1bafe49e8b05524
hubert322 <hubert322@gmail.com>
2019-08-17 13:18:13 +0800

    Add react router for future use

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import Home from "../Home/Home";
 import NavBar from "../NavBar/NavBar";
 import About from "../About/About";
