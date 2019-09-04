40aae6fc5908be1775e22b3c61971b4a568dc951
hubert322 <hubert322@gmail.com>
2019-09-04 10:53:34 -0400

    Added links to each contact and added a text msg

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -12,22 +12,42 @@ import "../../assets/styles/section.css";
 
 function Contact(props) {
   const { setRef } = props;
+  const contactText = `I am currently looking for an internship for summer 2020. 
+    Whether if you just want to chat about technology, want to know me better, 
+    or provide me with an opportunity, feel free to contact me via:
+  `;
 
   return (
     <div className="section-container" ref={ref => setRef("Contact", ref)}>
       <h2 className="section-title">Contact</h2>
       <div className="container row flex-wrap">
-        <div className="col-8">
-          <IconContext.Provider value={{ size: "5em" }}>
+        <h3>{contactText}</h3>
+        <IconContext.Provider value={{ size: "5em", color: "white" }}>
+          <a
+            className="Contact-link"
+            href="https://github.com/hubert322"
+            target="blank"
+          >
             <FaGithub />
+          </a>
+          <a
+            className="Contact-link"
+            href="https://www.linkedin.com/in/tzu-fan-hubert-hung"
+            target="blank"
+          >
             <FaLinkedin />
+          </a>
+          <a
+            className="Contact-link"
+            href="https://www.instagram.com/huberthung322"
+            target="blank"
+          >
             <FaInstagram />
+          </a>
+          <a className="Contact-link" href="mailto:huberth@umich.edu">
             <FaRegEnvelope />
-          </IconContext.Provider>
-        </div>
-        <div className="col-4">
-          <p>HELLOHELLO</p>
-        </div>
+          </a>
+        </IconContext.Provider>
       </div>
     </div>
   );
