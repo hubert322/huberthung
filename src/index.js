406b228a04d72b05eedaee17a45c65ca09184dd3
hubert322 <hubert322@gmail.com>
2019-09-05 19:03:14 -0400

    Remove router

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import "bootstrap/dist/css/bootstrap.css";
 import App from "./components/App/App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+// ReactDOM.render(
+//   <BrowserRouter basename="/">
+//     <App />
+//   </BrowserRouter>,
+//   document.getElementById("root")
+// );
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
