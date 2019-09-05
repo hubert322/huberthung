0f766314be15baf6557bba89ade6992e83549ca6
hubert322 <hubert322@gmail.com>
2019-09-05 19:12:38 -0400

    Updates

---
+++ precache-manifest.0b0353c2b66eda470a14d10fc7464b15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d5427ea3ccdba6fcb3c205e67d92dad",
+    "url": "huberthung.me/index.html"
+  },
+  {
+    "revision": "887b8168d581fba76990",
+    "url": "huberthung.me/static/css/2.8b8befd2.chunk.css"
+  },
+  {
+    "revision": "6b4629f237e5e2c61db3",
+    "url": "huberthung.me/static/css/main.a4e5630a.chunk.css"
+  },
+  {
+    "revision": "887b8168d581fba76990",
+    "url": "huberthung.me/static/js/2.13b446b4.chunk.js"
+  },
+  {
+    "revision": "6b4629f237e5e2c61db3",
+    "url": "huberthung.me/static/js/main.714a8ea6.chunk.js"
+  },
+  {
+    "revision": "60784395d6949a13094b",
+    "url": "huberthung.me/static/js/runtime~main.7a83dc20.js"
+  },
+  {
+    "revision": "b263d7a8e369c03344c526507cf1397a",
+    "url": "huberthung.me/static/media/profile.b263d7a8.jpg"
+  }
+]);
