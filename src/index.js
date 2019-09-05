5f4ffd471cdbb7494f7f6f4e4b445934b4b48497
hubert322 <hubert322@gmail.com>
2019-09-05 18:40:37 -0400

    Allow host on domain

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from "./components/App/App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>,
   document.getElementById("root")
